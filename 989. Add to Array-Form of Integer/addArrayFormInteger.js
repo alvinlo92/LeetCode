edf742f7307ee8dd6911356a1e2e6c968f099652
alvinlo92 <alvinlo92@yahoo.com>
2020-03-07 23:37:07 -0800

    complete addArrayFormInteger

--- 989. Add to Array-Form of Integer/addArrayFormInteger.js
+++ 989. Add to Array-Form of Integer/addArrayFormInteger.js
@@ -4,22 +4,15 @@
  * @return {number[]}
  */
 var addToArrayForm = function(A, K) {
-  for (let i = A.length - 1; i >= 0; i--) {
-      let sum = K + A[i];
-      
-      if (sum >= 10) {
-          A[i] = sum % 10;
-          K = (sum - A[i]) / 10;
-      } else {
-          A[i] = sum;
-          K = 0;
-      }
-      
-      if (i === 0 && sum >= 10) {
-          A.unshift(0);
-          i = 1;
-      }
-  }
-  
-  return A;
+    let tol = [];
+    for (let i = A.length - 1; i >= 0; i--) {
+        let sum = K + +A[i];
+        tol.push((sum % 10) + '');
+        K = Math.floor(sum / 10);
+        if (i === 0 && K > 0) {
+            A.unshift("0");
+            i = 1;
+        }
+    }
+    return tol.reverse();
 };
