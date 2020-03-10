a65154a811354830a628ea697a4149b79da41466
alvinlo92 <alvinlo92@yahoo.com>
2020-03-09 17:54:43 -0700

    complete containsDuplicate

---
+++ 217. Contains Duplicate/containsDuplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  let hash = {}
+  for (let i = 0; i < nums.length; i++) {
+      if (hash[nums[i]]) {
+          return true;
+      } else {
+          hash[nums[i]] = 1;
+      }
+  }
+  return false;
+};
