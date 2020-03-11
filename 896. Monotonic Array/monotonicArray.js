770c3c96201b88185d8aec07f17c65aa92fda1d9
alvinlo92 <alvinlo92@yahoo.com>
2020-03-11 14:17:37 -0700

    complete monotonicArray

---
+++ 896. Monotonic Array/monotonicArray.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ */
+var isMonotonic = function(A) {
+  let inc = true, dec = true;
+  for (let i = 0; i < A.length; i++) {
+      if (A[i] > A[i + 1]) inc = false;
+      if (A[i] < A[i + 1]) dec = false;
+  }
+  return inc || dec;
+};
