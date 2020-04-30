1997d71c89e4476954fedeb9abe87485f099e685
alvinlo92 <alvinlo92@yahoo.com>
2020-04-29 18:46:50 -0700

    optimized removeDuplicatesSortedArray

--- 26. Remove Duplicates from Sorted Array/removeDuplicatesSortedArray.js
+++ 26. Remove Duplicates from Sorted Array/removeDuplicatesSortedArray.js
@@ -3,16 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  var remover = function(start, end) {
-      if (nums[start] === nums[end]) {
-          return remover(start, end + 1);
-      } else {
-          return end - start - 1;
-      }
-  }
-  for (let i = 0; i < nums.length; i++) {
-      let delCnt = remover(i, i + 1);
-      nums.splice(i, delCnt);
-  }
-  return nums.length;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    return nums.length;
 };
