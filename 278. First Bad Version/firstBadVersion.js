294a31b7e80d11ba45722087c7366debfaad397f
alvinlo92 <alvinlo92@yahoo.com>
2020-05-16 22:49:57 -0700

    complete firstBadVersion

---
+++ 278. First Bad Version/firstBadVersion.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function(n) {
+      let left = 0;
+      let right = n;
+      while (left < right) {
+          let mid = Math.ceil((right - left) / 2 + left);
+          if (mid === right) break;
+          isBadVersion(mid) ? right = mid : left = mid;
+      }
+      return right;
+  };
+};
