5e08ce6f162c840a24284c7a7485fce1f76f509b
alvinlo92 <alvinlo92@yahoo.com>
2020-07-16 20:27:52 -0700

    complete pow(x, n)

---
+++ 50. Pow(x, n)/pow(x, n).js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if (n === 0) return 1;
+  if (n === 1) return x;
+  if (n < 0 && x !== 0) return myPow(1 / x, n *- 1);
+  if (n % 2 === 0) {
+      let res = myPow(x, n / 2);
+      return res * res;
+  } else {
+      return myPow(x, n - 1) * x;
+  }
+};
