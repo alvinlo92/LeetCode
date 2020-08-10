c6dc851b2389fa63ede1d21fdbcb4fd4bb48d0e9
alvinlo92 <alvinlo92@yahoo.com>
2020-08-10 15:45:19 -0700

    complete diameterOfBinaryTree

---
+++ 543. Diameter of Binary Tree/diameterOfBinaryTree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+  let ans = 1;
+  var depth = function(node) {
+      if (node === null) return 0;
+      let l = depth(node.left);
+      let r = depth(node.right);
+      ans = Math.max(ans, l + r + 1);
+      return Math.max(l, r) + 1;
+  }
+  depth(root);
+  return ans - 1;
+};
